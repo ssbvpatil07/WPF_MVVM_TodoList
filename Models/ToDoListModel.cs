ae80d1fd709ee7ca2bebb54ef8c37173ba27b84e
ssbvpatil07 <72382928+ssbvpatil07@users.noreply.github.com>
2025-06-27 16:30:59 +0530

    Add project files.

---
+++ Models/ToDoListModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Models
+{
+    public class ToDoListModel
+    {
+        public string Task { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ ViewModels/ToDoList_ViewModel.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+using TodoList.Models;
+
+namespace TodoList.ViewModels
+{
+    public class ToDoList_ViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<ToDoListModel> Tasks { get; set; } = new ObservableCollection<ToDoListModel>();
+
+        private string _newTask;
+        public string NewTask
+        {
+            get => _newTask;
+            set { _newTask = value; OnPropertyChanged(nameof(NewTask)); }
+        }
+
+        public ICommand AddCommand { get; }
+        public ICommand DeleteCommand { get; }
+
+        public ToDoList_ViewModel()
+        {
+            AddCommand = new RelayCommand(_ => AddTask(), _ => !string.IsNullOrWhiteSpace(NewTask));
+            DeleteCommand = new RelayCommand(DeleteTask);
+        }
+
+        private void AddTask()
+        {
+            Tasks.Add(new ToDoListModel { Task = NewTask, IsCompleted = false });
+            NewTask = string.Empty;
+        }
+
+        private void DeleteTask(object parameter)
+        {
+            if (parameter is ToDoListModel item)
+                Tasks.Remove(item);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
